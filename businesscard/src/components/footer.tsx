0ebb979269186e58871b0a3f1f85d4d89e67b435
LRCozier <lrcozier@gmail.com>
2025-04-28 16:36:48 +0100

    added social links

--- businesscard/src/components/footer.tsx
+++ businesscard/src/components/footer.tsx
@@ -9,10 +9,10 @@ import {
 const Footer = () => {
   return (
     <ul>
-      <li><FontAwesomeIcon icon={faInstagram} /></li>
-      <li><FontAwesomeIcon icon={faTiktok} /></li>
-      <li><FontAwesomeIcon icon={faGithub} /></li>
-      <li><FontAwesomeIcon icon={faLinkedin} /></li>
+      <a href='https://www.instagram.com/lacozierfitness/profilecard/?igsh=MXFrdHE0b24wMGlseA==' target='_blank'><li><FontAwesomeIcon icon={faInstagram} /></li></a>
+      <a href='https://www.tiktok.com/@lacozierfitness?_t=ZN-8vugAgLd42o&_r=1' target='_blank'><li><FontAwesomeIcon icon={faTiktok} /></li></a>
+      <a href='https://github.com/LRCozier' target='_blank'><li><FontAwesomeIcon icon={faGithub} /></li></a>
+      <a href='https://www.linkedin.com/in/luke-rudderham-cozier-30205343/' target='_blank'><li><FontAwesomeIcon icon={faLinkedin} /></li></a>
     </ul>
   );
 };
