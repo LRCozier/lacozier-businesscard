dfea57a4c58d6342b0141c0cc49d7f80689031c1
LRCozier <lrcozier@gmail.com>
2025-04-28 17:14:36 +0100

    added classname for socials

--- businesscard/src/components/footer.tsx
+++ businesscard/src/components/footer.tsx
@@ -8,7 +8,7 @@ import {
 
 const Footer = () => {
   return (
-    <ul>
+    <ul className='socials'>
       <a href='https://www.instagram.com/lacozierfitness/profilecard/?igsh=MXFrdHE0b24wMGlseA==' target='_blank'><li><FontAwesomeIcon icon={faInstagram} /></li></a>
       <a href='https://www.tiktok.com/@lacozierfitness?_t=ZN-8vugAgLd42o&_r=1' target='_blank'><li><FontAwesomeIcon icon={faTiktok} /></li></a>
       <a href='https://github.com/LRCozier' target='_blank'><li><FontAwesomeIcon icon={faGithub} /></li></a>
