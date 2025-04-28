0a0dd8d13124bdbbbd807454db91bac1322c5030
LRCozier <lrcozier@gmail.com>
2025-04-28 16:36:39 +0100

    updated contents

--- businesscard/src/components/about.tsx
+++ businesscard/src/components/about.tsx
@@ -7,6 +7,7 @@ const About = () => {
     <section className="section-about">
     <h1>Luke Rudderham-Cozier</h1>
     <h2>Front End Engineer</h2>
+    <p>A results-driven professional consistently upholding high standards of excellence across all areas of work.</p>
     <div className="buttons-row">
     <button className="btn" onClick={() => window.open()}>Developer Portfolio</button>
     <button className="btn" onClick={() => window.open()}>Personal Traiing</button>
