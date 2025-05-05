8994175fc311ae33271a0a4ad334399d0c73198f
LRCozier <lrcozier@gmail.com>
2025-05-05 11:48:21 +0100

    installed vite static copy

--- businesscard/vite.config.ts
+++ businesscard/vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/lacozier-businesscard",
-  
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'public', 'data.json'),
+          dest: './',
+        },
+      ],
+    }),
+  ],
+  base: '/lacozier-businesscard/',
+});
