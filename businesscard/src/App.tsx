f2a6493c7ed1221b02a29607a5fada52e6413fb2
LRCozier <lrcozier@gmail.com>
2025-07-14 11:18:56 +0100

    removed components from app.tsx

--- businesscard/src/App.tsx
+++ businesscard/src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css'
-import About from './components/about'
-import Connect from './components/connect'
-import ShareCard from './components/share'
 
 const App = () => {
 
   return (
     <>
     <main>
-      <About/>
-      <Connect/>
-      <ShareCard />
     </main>
     </>
   )
