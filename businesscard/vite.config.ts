98d4b6497c2ecb2da4e75d174036ef71ca458870
LRCozier <lrcozier@gmail.com>
2025-08-26 19:35:30 +0100

    updated vite config for deploy

--- businesscard/vite.config.ts
+++ businesscard/vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import path from 'path';
 
 export default defineConfig({
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: path.resolve(__dirname, 'public', 'data.json'),
-          dest: './',
-        },
-      ],
-    }),
   ],
   base: '/lacozier-businesscard/',
 });
